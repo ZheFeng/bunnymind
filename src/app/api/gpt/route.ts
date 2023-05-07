d724950686664651e78d3e55dcd4f42c7ae46d2d
Zhe Feng <fengzhe1983@gmail.com>
2023-05-07 11:32:30 +0800

    create basic api

--- src/app/api/gpt/route.ts
+++ src/app/api/gpt/route.ts
@@ -7,8 +7,20 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export async function GET(request: Request) {
+  if (!configuration.apiKey) {
+    return NextResponse.error();
+  }
+
   const { searchParams } = new URL(request.url);
   const prompt = searchParams.get("prompt");
+  if (prompt === null || prompt.trim().length === 0) {
+    return NextResponse.error();
+  }
 
-  return NextResponse.json({ prompt });
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    temperature: 0.6,
+  });
+  return NextResponse.json({ result: completion.data });
 }

c1104f600cd0b960cc90941e92213004d335431d
Zhe Feng <fengzhe1983@gmail.com>
2023-05-26 23:52:26 +0800

    create form

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1 className="display-1">Bunnymind</h1>
-    </div>
+    <main className="container mt-5">
+      <div>
+        <form className="mb-5">
+          <div className="form-floating mb-3">
+            <input
+              className="form-control"
+              onChange={(e: React.FormEvent<HTMLInputElement>) => {}}
+              placeholder="What do you want to ask?"
+              type="text"
+              name="prompt"
+              id="prompt"
+            />
+            <label htmlFor="prompt">What do you want to ask?</label>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }
 
